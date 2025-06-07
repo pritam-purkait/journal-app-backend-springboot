620a1d0313f77337700f10c94f395b65bed10fac
pritam purkait <pritampurkait5533@gmail.com>
2025-06-07 06:42:21 +0530

    Lombok

--- src/main/java/com/pritam/journalApp/entity/JournalEntry.java
+++ src/main/java/com/pritam/journalApp/entity/JournalEntry.java
@@ -1,6 +1,7 @@
 package com.pritam.journalApp.entity;
 
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
 
     @Id
@@ -17,35 +19,4 @@ public class JournalEntry {
     private String content;
     private LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
