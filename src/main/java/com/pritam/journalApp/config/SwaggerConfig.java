b2b526f0758ec944680e278a2b7173918277a363
pritam-purkait <pritampurkait5533@gmail.com>
2025-07-14 23:35:42 +0530

    fix():swagger config

--- src/main/java/com/pritam/journalApp/config/SwaggerConfig.java
+++ src/main/java/com/pritam/journalApp/config/SwaggerConfig.java
@@ -1,16 +1,11 @@
 package com.pritam.journalApp.config;
 
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
-import java.util.List;
+
 
 @Configuration
 public class SwaggerConfig {
