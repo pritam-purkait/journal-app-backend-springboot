1a9f98efd4109e675a2818217f71e388824d8cee
pritam-purkait <pritampurkait5533@gmail.com>
2025-07-14 13:08:50 +0530

    fix(): fix Home controller

--- src/main/java/com/pritam/journalApp/controller/HomeController.java
+++ src/main/java/com/pritam/journalApp/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home(){
-        return "redirect:/swagger-ui/index.html";
+        return "redirect:/journal/swagger-ui/index.html";
     }
 }
